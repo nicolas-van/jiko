39f7a161f4d99c5dda673773ec5651879c30dbf2
Nicolas Vanhoren <nicolas.vanhoren.01@gmail.com>
2013-11-28 17:58:46 +0100

    Corrected problem in ie

--- test_server_compilation.js
+++ test_server_compilation.js
@@ -4,16 +4,20 @@
 (function() {
 "use strict";
 
+var trim = function(t) {
+    return t.replace(/^\s+|\s+$/g, '');
+};
+
 describe("Jiko Server-side Compilation", function() {
 
 it("function", function() {
     var res = exfunction();
-    assert.equal(res.trim().replace(/\s+/g, " "), "Hello World");
+    assert.equal(trim(res).replace(/\s+/g, " "), "Hello World");
 });
 
 it("module", function() {
     var res = exmodule.func2();
-    assert.equal(res.trim().replace(/\s+/g, " "), "<div> zzz yyy");
+    assert.equal(trim(res).replace(/\s+/g, " "), "<div> zzz yyy");
 });
 
 });
